fb8b4fd6ddf6332c5bc07ce0e6e63f616ec03ffb
Anaaayyy <anna.bukshtynova251106@gmail.com>
2024-09-27 18:49:25 +0300

    Элементы между минимальным и максимальным

--- Program.cs
+++ Program.cs
@@ -10,6 +10,34 @@ namespace _1_2_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-----------------Элементы между минимальным и максимальным----------------");
+            Console.Write("Введите размер массива K: ");
+            int K = int.Parse(Console.ReadLine()); // Вводит размер массива
+            Console.Write("Введите диапазон [A, B): ");
+            int A = int.Parse(Console.ReadLine()); // Вводит начало диапазона
+            int B = int.Parse(Console.ReadLine()); // Вводит конец диапазона
+
+            // Создаёт массив случайных чисел
+            Random rand = new Random();
+            int[] array = Enumerable.Range(0, K).Select(_ => rand.Next(A, B)).ToArray();
+
+            // Находит индексы минимального и максимального элементов
+            int minIndex = Array.IndexOf(array, array.Min());
+            int maxIndex = Array.IndexOf(array, array.Max());
+
+            Console.WriteLine("Массив: " + string.Join(", ", array));
+
+            // Определяет диапазон индексов и выводит элементы между ними
+            int start = Math.Min(minIndex, maxIndex);
+            int end = Math.Max(minIndex, maxIndex);
+
+            Console.WriteLine("Элементы между минимальным и максимальным: ");
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadKey();
+
         }
     }
 }
